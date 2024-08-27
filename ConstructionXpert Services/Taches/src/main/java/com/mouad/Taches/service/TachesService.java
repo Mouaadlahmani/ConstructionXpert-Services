eca4f0189266e3d524b4a5901753d12335c5b76d
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-27 17:40:47 +0100

    Creation de TachesService

---
+++ ConstructionXpert Services/Taches/src/main/java/com/mouad/Taches/service/TachesService.java
@@ -0,0 +1,14 @@
+package com.mouad.Taches.service;
+
+import com.mouad.Taches.model.Taches;
+
+import java.util.List;
+
+public interface TachesService {
+    Taches ajouterTache(Taches taches);
+    Taches editTache(Long id, Taches taches);
+    List<Taches> getAll();
+    void deleteTaches(Long id);
+    Taches changerStatut(Long id, Taches taches);
+    List<Taches> getAllTachesByProjet(Long id);
+}
