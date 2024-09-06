4836bd9dd57dbafdc0725a817ede78147e4e63e8
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-06 16:52:53 +0100

    Creation de AuthenticationController

---
+++ ConstructionXpert Services/security/src/main/java/com/mouad/security/controller/AuthenticationController.java
@@ -0,0 +1,43 @@
+package com.mouad.security.controller;
+
+import com.mouad.security.dto.AuthenticationRequest;
+import com.mouad.security.dto.AuthenticationResponse;
+import com.mouad.security.dto.RegisterRequest;
+import com.mouad.security.service.AuthenticationService;
+import com.mouad.security.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    private final AuthenticationService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ){
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ){
+        return ResponseEntity.ok(authService.authenticate(request));
+
+    }
+
+    @GetMapping("/validate")
+    public String validateToken(@RequestParam("token") String token) {
+        jwtService.validateToken(token);
+        return "Token is valid";
+    }
+}
